5cc3e1668d032b53437d9c8cd42a27c9c62d5f84
Loïc <hello@loichorellou.net>
2019-08-28 12:48:27 +0200

    Update info-dossier.php

--- info-dossier.php
+++ info-dossier.php
@@ -3,17 +3,12 @@
 
 $dossier = $_GET['dossier'];
 
-
 foreach( GLOB("projets/$dossier/images/*.{jpg,png,gif}", GLOB_BRACE) as $image ){
 
 	echo "<li><img src='$image'></li>\n";
-
 }
-
-
 $texte = file_get_contents( "projets/".$dossier."/texte.html" );
 
 echo "<li class='texte'><div>$texte</div></li>\n";
-
 ?>
 </ul>
